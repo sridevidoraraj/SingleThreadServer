27ece035285587af5d24228209a7bef122677591
Sridevi <sridevi.elangovan@gcitsolutions.com>
2023-02-03 18:13:10 +0530

    *. changed time duration between request and response *. added logger

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
@@ -11,26 +8,34 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class Demo {
-    static Logger logger = Logger.getLogger(Demo.class.getName());
+    static ColorLogger colorLogger = new ColorLogger();
+    static {
+        InputStream stream = Demo.class.getClassLoader().
+                getResourceAsStream("logging.properties");
+        try {
+            LogManager.getLogManager().readConfiguration(stream);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public static void main( String[] args ) throws Exception {
         try (ServerSocket serverSocket = new ServerSocket(8085)){
-            logger.info("Server Started ");
-            Instant start = Instant.now();
-            logger.info("Starting Time : " +start);
-
             while (true) {
-                try (Socket client = serverSocket.accept()) {
-                    handleClient(client);
-                    Instant end = Instant.now();
-                    logger.info("End Time : " +end);
-                    long elapsedTime = Duration.between(start,end).toMillis();
-                    logger.info("Duration of Request and Response : " +elapsedTime+"milliseconds");
-//                    String formattedElapsedTime = String.format("%02d:%02d:%02d", elapsedTime.toHoursPart(), elapsedTime.toMinutesPart(),
-//                            elapsedTime.toSecondsPart());
-//                    logger.info("Formatted Time Duration : " +formattedElapsedTime);
+                colorLogger.logInfo("Server Started ");
+            try (Socket client = serverSocket.accept()) {
+                Instant start = Instant.now();
+                colorLogger.logInfo("Starting Time : " +start);
+                handleClient(client);
+                Instant end = Instant.now();
+                colorLogger.logInfo("End Time : " +end);
+                long elapsedTime = Duration.between(start,end).toMillis();
+                colorLogger.logInfo("Duration of Request and Response : " +elapsedTime+ " milliseconds");
                 }
             }
         }
@@ -82,7 +87,7 @@ public class Demo {
 
     private static void sendResponse(Socket client, String status, String contentType, byte[] content) throws IOException {
         OutputStream clientOutput = client.getOutputStream();
-        clientOutput.write(("HTTP/1.1 \r\n" + status+"\n").getBytes());
+        clientOutput.write(("HTTP/1.1 " + status+"\r\n").getBytes());
         clientOutput.write(("ContentType: " + contentType + "\r\n").getBytes());
         clientOutput.write("\r\n".getBytes());
         clientOutput.write(content);
